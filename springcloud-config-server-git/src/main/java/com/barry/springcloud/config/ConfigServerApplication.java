964cf8233b27d8c7641c0730a1c7cc188cf0cf60
QXV0963 <Tongshan.Han @partner. bmw.com>
2019-09-05 16:40:30 +0800

    init pom

---
+++ springcloud-config-server-git/src/main/java/com/barry/springcloud/config/ConfigServerApplication.java
@@ -0,0 +1,12 @@
+package com.barry.springcloud.config;
+
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+/**
+ * @description:
+ * @author: Tongshan.Han
+ * @time: 2019/9/5 16:26
+ */
+@SpringBootApplication
+public class ConfigServerApplication {
+}
